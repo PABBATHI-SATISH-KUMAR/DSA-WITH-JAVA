6f301a6893b2bb7913ddc1ba4ba3771382ce5263
PABBATHI-SATISH-KUMAR <pabbathisatishkumar@gmail.com>
2024-07-25 22:51:36 +0530

    Functions is modified

--- Functions.java
+++ Functions.java
@@ -130,6 +130,7 @@ public boolean isPrime(int n){
         }
         return true;
 }
+
 // 8)Write a program that will ask the user to enter his/her marks (out of 100). Define a method that will display grades according to the marks entered as below:
         // Marks        Grade 
         // 91-100         AA
