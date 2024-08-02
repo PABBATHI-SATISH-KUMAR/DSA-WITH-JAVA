eb4287c4aa785a17f81ec89c61b41f8341fa68a6
PABBATHI-SATISH-KUMAR <pabbathisatishkumar@gmail.com>
2024-08-02 07:19:11 +0530

    Inheritance.java is modified

--- Inheritance.java
+++ Inheritance.java
@@ -5,29 +5,14 @@ public class Inheritance {
         Rahul r1 = new Rahul();
         r1.trip();
         r1.property();
+        //r1.showPercentage();
         Chaitanya c1 = new Chaitanya();
         c1.trip();
         //c1.property();
         c1.gym();
+        //c1.go();
     }
 }
-// class Shashank{
-//     public void trip(){
-//         System.out.println("Lets go for trip");
-//     }
-// }
-// class Rahul extends Shashank{
-//     void property(){
-//         System.out.println("lets go for park");
-//     }
-// }
-// class Chaitanya extends Rahul{
-//     void gym(){
-//         System.out.println("Lets go for gym");
-//     }
-
-// }
-
 class Shashank{
     public void trip(){
         System.out.println("Lets go for trip");
@@ -38,8 +23,39 @@ class Rahul extends Shashank{
         System.out.println("lets go for park");
     }
 }
-class Chaitanya extends Shashank{
+class Chaitanya extends Rahul{
     void gym(){
         System.out.println("Lets go for gym");
     }
+
 }
+
+// class Shashank{
+//     int percentage = 78;
+//     public void trip(){
+//         System.out.println("Lets go for trip");
+//     }
+// }
+// class Rahul extends Shashank{
+//     void property(){
+//         System.out.println("lets go for park");
+//     }
+//     void showPercentage() {
+//         // Accessing percentage from the superclass
+//         System.out.println("Rahul's percentage is: " + percentage);
+//     }
+
+// }
+// class Chaitanya extends Shashank{
+//     void gym(){
+//         System.out.println("Lets go for gym");
+//     }
+//     void go(){
+//         System.out.println("lets go for go");
+//     }
+//     void showPercentage() {
+//         // Accessing percentage from the superclass
+//         System.out.println("Chaitnaya's percentage is: " + percentage);
+//     }
+// }
+
