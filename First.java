dbce224c45ee2773c67be3ce20e4a947603a4db1
PABBATHI-SATISH-KUMAR <pabbathisatishkumar@gmail.com>
2025-04-10 22:17:09 +0530

    Some changes made

--- First.java
+++ First.java
@@ -9,117 +9,111 @@
 // 9)To find Armstrong Number between two given number.
 
 import java.util.Scanner;
+
 public class First {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // int n = sc.nextInt();
-        //String s = sc.nextLine();
-        int p = sc.nextInt();
-        int t = sc.nextInt();
-        int r = sc.nextInt();
-        // int num1 = sc.nextInt();
-        // int num2 = sc.nextInt();
-        //int n = sc.nextInt();
-       
-        First a = new First();
-        
 
+        // Uncomment below lines to take user input for largeNum function
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        First a = new First();
+
+        int ans = a.largeNum(num1, num2);
+        System.out.println("Largest number is: " + ans);
+
+        sc.close();
+
+        // Examples (Don't run these now):
+        // int n = sc.nextInt();
+        // String s = sc.nextLine();
+        // int p = sc.nextInt();
+        // int t = sc.nextInt();
+        // int r = sc.nextInt();
+        // int n = sc.nextInt();
         // int start = sc.nextInt();
         // int end = sc.nextInt();
-        //  a.noOfArmstrongs(start, end);
-        
-        
-  
+        // a.noOfArmstrongs(start, end);
         // int ans = a.fibonacci(n);
         // System.out.println(ans);
         // double ans = a.convertToUSD(n);
         // System.out.println(ans);
-        // // int ans = a.largeNum(num1,num2);
+        // double ans = a.simpleIntrest(p,t,r);
         // System.out.println(ans);
-        double ans = a.simpleIntrest(p,t,r);
-        System.out.println(ans);
         // String ans = a.greeting(s);
         // System.out.println(ans);
-        //String ans = a.isOddEven(n);
-        //System.out.println(ans);
-        sc.close();
+        // String ans = a.isOddEven(n);
+        // System.out.println(ans);
     }
 
-    // 1)Write a program to print whether a number is even or odd, also take input from the user.
-    public String isOddEven(int n){
-        
-        if((n&1)==0){
+    // 1)Write a program to print whether a number is even or odd
+    public String isOddEven(int n) {
+        if ((n % 2) == 0) {
             return "Even";
-        }
-        else{
+        } else {
             return "Odd";
         }
     }
 
-    // 2)Take name as input and print a greeting message for that particular name.
-    public String greeting(String s){
-        return "Hello "+s+"! How are you?";
+    // 2)Greeting message
+    public String greeting(String s) {
+        return "Hello " + s + "! How are you?";
     }
-    // 3)Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
-    public double simpleIntrest(int p,int t,int r){
-        return (p*t*r)/100;
-    }
-    // 4)Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
-    // public int calculate(int a,int b){
-        
 
-    // }
-    
-    // 5)Take 2 numbers as input and print the largest number.
-    public int largeNum(int a,int b){
-        if(a>b){
+    // 3)Simple Interest
+    public double simpleIntrest(int p, int t, int r) {
+        return (p * t * r) / 100;
+    }
+
+    // 5)Print the largest number
+    public int largeNum(int a, int b) {
+        if (a > b) {
             return a;
-        }
-        else{
+        } else {
             return b;
         }
     }
-   
-    // 6)Input currency in rupees and output in USD.
-    public double convertToUSD(int n){
-        return 0.012*n;
+
+    // 6)Convert to USD
+    public double convertToUSD(int n) {
+        return 0.012 * n;
     }
-    // 7)To calculate Fibonacci Series up to n numbers.
-    public int fibonacci(int n){
+
+    // 7)Fibonacci Series up to n numbers
+    public int fibonacci(int n) {
         int a = 0;
         int b = 1;
         int count = 2;
-        while (count<=n){
+        while (count <= n) {
             int temp = b;
-            b=a+b;
+            b = a + b;
             a = temp;
             count++;
         }
         return b;
     }
 
-    // 9)To find Armstrong Number between two given number.
-
-    public void noOfArmstrongs(int start,int end){
-        for(int i=start;i<=end;i++){
-            if(isArmstrong(i)){
-                System.out.println(i);            }
+    // 9)Armstrong numbers between two values
+    public void noOfArmstrongs(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isArmstrong(i)) {
+                System.out.println(i);
+            }
         }
-        
     }
 
-    public boolean isArmstrong(int n){
+    public boolean isArmstrong(int n) {
         int original = n;
         double sum = 0;
-        while(n>0){
-            int rem = n%10;
-            n=n/10;
-            sum =  sum + Math.pow(rem,3);
+        while (n > 0) {
+            int rem = n % 10;
+            n = n / 10;
+            sum = sum + Math.pow(rem, 3);
         }
-        if(sum==original){
-            return true;
-        }
-        return false;
+        return sum == original;
     }
 }
