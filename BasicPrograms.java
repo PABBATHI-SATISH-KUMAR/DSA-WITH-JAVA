d8a6af3a2c7d0b8a306f5c83755b949ce552790f
PABBATHI-SATISH-KUMAR <pabbathisatishkumar@gmail.com>
2025-06-17 05:16:56 +0530

    Practicing basic problems in java

--- BasicPrograms.java
+++ BasicPrograms.java
@@ -47,17 +47,46 @@ public class BasicPrograms {
             System.out.println(n + " is not a prime number.");
         }
         // 3) Palindrome Program in Java
+        
+    }
+    public void Palindrome(int n){
+        int temp = n;
+        int sum = 0;
+        while(n>0){
+            int rem = n%10;
+            sum = (sum*10) + rem;
+            n/=10;
+        }
+        if(temp==sum){
+            System.out.println(temp+"is a palindrome");
+        }
+        else{
+            System.out.println(temp+"is not a palindrome number");
+        }
     }
     public static void main(String[] args) {
         BasicPrograms bp = new BasicPrograms();
-        int n = 10; // Example input for Fibonacci series
-        System.out.println("Fibonacci series up to " + n + " terms: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(bp.fibonacciseries(i) + " ");
-        }
-        System.out.println();
-        int primeCheck = 29; 
-        bp.prime_number(primeCheck);
+        // int n = 10; // Example input for Fibonacci series
+        // System.out.println("Fibonacci series up to " + n + " terms: ");
+        // for (int i = 0; i < n; i++) {
+        //     System.out.print(bp.fibonacciseries(i) + " ");
+        // }
+        // System.out.println();
+        // int primeCheck = 29; 
+        // bp.prime_number(primeCheck);
+        int n = 121;
+        bp.Palindrome(n);
+        
+        // int n2 = 123;
+        // bp.Palindrome(n2);
+        
+        // int n3 = 0;
+        // bp.Palindrome(n3); // Edge case for 0
+        
+        // int n4 = 5;
+        // bp.Palindrome(n4); // Single digit
+        
+
     }
 
 
