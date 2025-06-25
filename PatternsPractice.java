6f5cc4648df76d70081d19a3c95a10d7b7f19f3f
PABBATHI-SATISH-KUMAR <pabbathisatishkumar@gmail.com>
2025-06-25 17:06:23 +0530

    Practicing Patterns

--- PatternsPractice.java
+++ PatternsPractice.java
@@ -26,13 +26,24 @@ class PatternsPracticeMain{
         }
     }
 
-    
+    public static void rightAngleTriangleNumbers(int n){
+        // 1
+        // 1 2 
+        // 1 2 3
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
         //nForest(n);
-        rightAngleTriangle(n);
+        //rightAngleTriangle(n);
+        rightAngleTriangleNumbers(n);
         sc.close();
 
     }
