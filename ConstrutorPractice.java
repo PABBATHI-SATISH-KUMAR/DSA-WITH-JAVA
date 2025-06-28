32c0e345328d145d1cf79ebc7bf7db1a09b2c594
PABBATHI-SATISH-KUMAR <pabbathisatishkumar@gmail.com>
2025-06-28 10:58:20 +0530

    Constructors

--- ConstrutorPractice.java
+++ ConstrutorPractice.java
@@ -4,8 +4,15 @@ class ConstructotPractice{
     // Constructor to initialize the variables
     ConstructotPractice(int x, int y) {
         a = x;
-        b = y;       
+        b = y;  
+        System.err.println("Constructor called with values: " + a + " and " + b);     
     }
+    ConstructotPractice(int x, long y) {
+        a = x;
+        b = (int) y;  
+        System.err.println("Constructor called with values: " + a + " and " + b);     
+    }
+
     // Method to add the two numbers
     int add() {
         return a + b;           
@@ -14,6 +21,7 @@ class ConstructotPractice{
     public static void main(String[] args) {
         // Create an object of the class and initialize it using the constructor
         ConstructotPractice obj = new ConstructotPractice(5, 10);
+        ConstructotPractice obj1 = new ConstructotPractice(55, 10);
         // Call the add method and print the result
         System.out.println("The sum is: " + obj.add());     
     }    
